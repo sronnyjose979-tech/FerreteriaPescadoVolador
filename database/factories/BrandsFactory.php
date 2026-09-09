05dd025d8e65cc351a6a2c2dbe0b55384e6bb756
Justin Solano <justinso.le979@gmail.com>
2026-09-09 14:35:46 -0600

    se hicieron los modelos,seeder,factories,migraton y controller de Product Category Brand Customer Customer_Address Shopping_cart Cart_Item User Role

---
+++ database/factories/BrandsFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Brands;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Brands>
+ */
+class BrandsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
